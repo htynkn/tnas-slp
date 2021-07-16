4449d3f8ae2a9bbe38986385c0c7190eff625b21
htynkn <htynkn@gmail.com>
2021-07-16 10:24:54 +0800

    add page and ci script

---
+++ packing/usr/www/mod/4.Application/100.slp.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Switch Lan Play Server</title>
+    
+</head>
+<body>
+    
+<h1>Switch Lan Play Server</h1>
+
+<p>Access server status by <a href="<?php
+                    $ip = $_SERVER['SERVER_ADDR'];
+                    echo "http://$ip:11451/";
+                    ?>" target="_blank">web page</a>
+                    </p>
+
+<p></p>
+
+<p>Find more information in website: <a href="https://github.com/spacemeowx2/slp-server-rust" target="_blank">slp-server-rust</a></p>
+<p>Notice: this app (source code) is created by spacemeowx2.</p>
+
+</body>
+</html>
